352858b8b4d48c09430b6d486e1ec84c49c67283
alianlbj23 <alianlbj23@gmail.com>
2024-08-14 04:34:37 +0800

    add depth data

--- src/yolo_pkg/yolo_pkg/pokemon_node.py
+++ src/yolo_pkg/yolo_pkg/pokemon_node.py
@@ -33,6 +33,7 @@ class PokemonNode(Node):
         self.rosbridge_client.run()
         self.coordinate_topic = roslibpy.Topic(self.rosbridge_client, '/object_coordinates', 'geometry_msgs/Point')
         self.direction_topic = roslibpy.Topic(self.rosbridge_client, '/object_direction', 'std_msgs/String')
+        self.depth_topic = roslibpy.Topic(self.rosbridge_client, '/object_depth', 'std_msgs/String')
 
         # 读取相机内参
         camera_calib_path = os.path.join(package_share_directory, 'resource', 'ost.yaml')
@@ -47,12 +48,13 @@ class PokemonNode(Node):
         # 创建发布者
         self.detection_image_publisher_ = self.create_publisher(Image, "yolo_detection_image", 10)
         self.detection_publisher_ = self.create_publisher(Detection, "yolo_detection_topic", 10)
+        self.color_image_publisher_ = self.create_publisher(Image, "object_color_image", 10)
         self.coordinate_publisher_ = self.create_publisher(Point, "object_coordinates", 10)  # 新的坐标发布者
 
         self.create_subscription(CompressedImage, "/camera/color/image_raw/compressed", self.image_callback, 10)
         self.create_subscription(CompressedImage, "/camera/depth/image_raw/compressed", self.depth_callback, 10)
         self.create_subscription(Imu, "/imu/data_raw", self.imu_callback, 10)
-
+        self.color_background = None
         self.depth_image = None
 
     def imu_callback(self, msg):
@@ -65,117 +67,121 @@ class PokemonNode(Node):
             orientation_q.w
         ])
         self.orientation_data.append(euler)
-        # self.plot_orientation()
-
-    def plot_orientation(self):
-        plt.clf()
-        plt.plot(range(len(self.orientation_data)), [d[0] for d in self.orientation_data], label='Roll')
-        plt.plot(range(len(self.orientation_data)), [d[1] for d in self.orientation_data], label='Pitch')
-        plt.plot(range(len(self.orientation_data)), [d[2] for d in self.orientation_data], label='Yaw')
-        plt.legend()
-        plt.pause(0.01)
 
     def image_callback(self, msg):
-        target_tags = ["pikachu",]  # 修改這裡來設置你想篩選的標籤
-        tolerance = 0.1  # 容忍範圍，可以根據需求調整
+        target_tag = "pikachu"  # 指定需要处理的标签
+        tolerance = 0.1  # 容忍度
 
         if isinstance(msg, CompressedImage):
-            # 解壓縮圖像數據
             np_arr = np.frombuffer(msg.data, np.uint8)
             frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
         else:
             frame = self.bridge.imgmsg_to_cv2(msg, desired_encoding='bgr8')
 
-        frame_height, frame_width = frame.shape[:2]
-        frame_center_x = frame_width / 2
-        frame_center_y = frame_height / 2
-
-        # 使用YOLO進行物體檢測，顯式指定使用GPU
         results = self.model.predict(source=frame, verbose=False, device='cuda')
+
+        color_background = np.zeros_like(frame)
 
         if results and len(results[0].boxes) > 0:
             for result in results:
                 for box in result.boxes:
-                    cls = box.cls[0]
-                    label = self.model.names[int(cls)]
+                    label = self.model.names[int(box.cls[0])]
+                    if label == target_tag:  # 只处理特定标签的物体
+                        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
 
-                    # 只處理特定標籤
-                    if label in target_tags:
-                        x1, y1, x2, y2 = box.xyxy[0].tolist()
-                        confidence = box.conf[0]
+                        object_region = frame[y1:y2, x1:x2]
+                        avg_color = cv2.mean(object_region)[:3]
 
-                        # 計算偵測框的中心點
-                        box_center_x = (x1 + x2) / 2
-                        box_center_y = (y1 + y2) / 2
-
-                        # 判断物体在相机视野中的方向
-                        direction = ""
-                        if abs(box_center_x - frame_center_x) / frame_width < tolerance:
-                            if abs(box_center_y - frame_center_y) / frame_height < tolerance:
-                                direction = "front"
-                            elif box_center_y < frame_center_y:
-                                direction = "up"
-                            else:
-                                direction = "down"
-                        elif box_center_x < frame_center_x:
-                            direction = "left"
-                        else:
-                            direction = "right"
-                        print(direction)
-                        direction_msg = roslibpy.Message({'data': direction})
-                        self.direction_topic.publish(direction_msg)
+                        color_block = np.full(object_region.shape, avg_color, dtype=np.uint8)
+                        color_background[y1:y2, x1:x2] = color_block
 
                         depth, point = self.get_depth_and_point_for_box(x1, y1, x2, y2)
+                        if depth is not None:
+                            depth_in_meters = depth / 1000.0
+                            print(depth_in_meters)
 
-                        if depth is not None and point is not None:
-                            # 使用內參矩陣將像素座標轉換為相機座標
-                            point_camera = np.dot(np.linalg.inv(self.camera_matrix), np.array([point[0], point[1], 1.0])) * (depth / 1000.0)  # 轉換為米
+                            # 发布深度信息到新的topic
+                            depth_msg = roslibpy.Message({'data': str(depth_in_meters)})
+                            self.depth_topic.publish(depth_msg)
 
-                            # 計算物體在世界座標系中的位置
-                            point_world = np.dot(self.extrinsics_matrix, np.array([point_camera[0], point_camera[1], point_camera[2], 1.0]))
+                            if depth_in_meters < 0.2:
+                                rect_width = 100
+                                rect_height = 100
+                                rect_center_x = frame.shape[1] // 2 + 20
+                                rect_center_y = frame.shape[0] // 2 + 100
 
-                            # 將相機座標轉換為PyBullet座標：PyBullet的x=相機的z，PyBullet的y=相機的x，PyBullet的z=相機的y
-                            point_bullet = np.array([point_world[2], -point_world[0], -point_world[1]])
+                                box_center_x = (x1 + x2) // 2
+                                box_center_y = (y1 + y2) // 2
 
-                            # 發佈檢測消息
-                            detection_msg = Detection()
-                            detection_msg.class_name = label
-                            detection_msg.confidence = float(confidence)
-                            detection_msg.xmin = float(x1)
-                            detection_msg.ymin = float(y1)
-                            detection_msg.xmax = float(x2)
-                            detection_msg.ymax = float(y2)
-                            detection_msg.depth = float(depth)
-                            detection_msg.point_x = float(point_bullet[0])
-                            detection_msg.point_y = float(point_bullet[1])
-                            detection_msg.point_z = float(point_bullet[2])
-                            self.detection_publisher_.publish(detection_msg)
+                                if abs(box_center_x - rect_center_x) < rect_width // 2 and abs(box_center_y - rect_center_y) < rect_height // 2:
+                                    direction = "front"
+                                elif box_center_x < rect_center_x - rect_width // 2:
+                                    direction = "left"
+                                elif box_center_x > rect_center_x + rect_width // 2:
+                                    direction = "right"
+                                elif box_center_y < rect_center_y - rect_height // 2:
+                                    direction = "up"
+                                else:
+                                    direction = "down"
+                            else:
+                                frame_center_x = frame.shape[1] / 2
+                                frame_center_y = frame.shape[0] / 2
+                                up_tolerance_factor = 2.0
+                                front_offset_factor = 0.5
 
-                            # 發佈座標消息
-                            point_msg = Point()
-                            point_msg.x = float(point_bullet[0])
-                            point_msg.y = float(point_bullet[1])
-                            point_msg.z = float(point_bullet[2])
-                            self.coordinate_publisher_.publish(point_msg)
+                                if abs((x1 + x2) / 2 - frame_center_x) / frame.shape[1] < tolerance:
+                                    if abs((y1 + y2) / 2 - (frame_center_y - frame.shape[0] * front_offset_factor)) / frame.shape[0] < tolerance:
+                                        direction = "front"
+                                    elif (y1 + y2) / 2 < frame_center_y - frame.shape[0] * (up_tolerance_factor * tolerance):
+                                        direction = "up"
+                                    else:
+                                        direction = "down"
+                                elif (x1 + x2) / 2 < frame_center_x:
+                                    direction = "left"
+                                else:
+                                    direction = "right"
 
-                            roslibpy_point_msg = roslibpy.Message({
-                                'x': float(point_bullet[0]),
-                                'y': float(point_bullet[1]),
-                                'z': float(point_bullet[2])
-                            })
-                            self.coordinate_topic.publish(roslibpy_point_msg)
+                            direction_msg = roslibpy.Message({'data': direction})
+                            self.direction_topic.publish(direction_msg)
+
+                            if point is not None:
+                                point_camera = np.dot(np.linalg.inv(self.camera_matrix), np.array([point[0], point[1], 1.0])) * depth_in_meters
+                                point_world = np.dot(self.extrinsics_matrix, np.array([point_camera[0], point_camera[1], point_camera[2], 1.0]))
+                                point_bullet = np.array([point_world[2], -point_world[0], -point_world[1]])
+
+                                point_msg = Point()
+                                point_msg.x = float(point_bullet[0])
+                                point_msg.y = float(point_bullet[1])
+                                point_msg.z = float(point_bullet[2])
+                                self.coordinate_publisher_.publish(point_msg)
+
+                                roslibpy_point_msg = roslibpy.Message({
+                                    'x': float(point_bullet[0]),
+                                    'y': float(point_bullet[1]),
+                                    'z': float(point_bullet[2])
+                                })
+                                self.coordinate_topic.publish(roslibpy_point_msg)
 
         annotated_frame = results[0].plot()
-        msg = self.bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
-        self.detection_image_publisher_.publish(msg)
 
+        rect_width = 100
+        rect_height = 100
+        rect_center_x = annotated_frame.shape[1] // 2 + 20
+        rect_center_y = annotated_frame.shape[0] // 2 + 100
 
+        top_left = (rect_center_x - rect_width // 2, rect_center_y - rect_height // 2)
+        bottom_right = (rect_center_x + rect_width // 2, rect_center_y + rect_height // 2)
 
+        cv2.rectangle(annotated_frame, top_left, bottom_right, color=(0, 255, 0), thickness=2)
 
+        detection_image = self.bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
+        self.detection_image_publisher_.publish(detection_image)
+
+        color_image = self.bridge.cv2_to_imgmsg(color_background, encoding="bgr8")
+        self.color_image_publisher_.publish(color_image)
 
     def depth_callback(self, msg):
         if isinstance(msg, CompressedImage):
-            # 解压缩图像数据
             np_arr = np.frombuffer(msg.data, np.uint8)
             self.depth_image = cv2.imdecode(np_arr, cv2.IMREAD_UNCHANGED)
         else:
