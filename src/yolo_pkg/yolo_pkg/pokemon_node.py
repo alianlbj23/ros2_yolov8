a94814012d089f2d8fcda25a6daca43969d56a4f
alianlbj23 <alianlbj23@gmail.com>
2024-08-18 19:40:46 +0800

    Crosshair to aim target

--- src/yolo_pkg/yolo_pkg/pokemon_node.py
+++ src/yolo_pkg/yolo_pkg/pokemon_node.py
@@ -25,15 +25,16 @@ class PokemonNode(Node):
         self.orientation_data = deque(maxlen=100)
 
         package_share_directory = get_package_share_directory('yolo_pkg')
-        model_path = os.path.join(package_share_directory, 'resource', 'best.pt')
+        model_path = os.path.join(package_share_directory, 'resource', 'best_large.pt')
         self.model = YOLO(model_path)
 
         # ROSBridge client to connect to Jetson
-        self.rosbridge_client = roslibpy.Ros(host='192.168.0.207', port=9090)
+        self.rosbridge_client = roslibpy.Ros(host='192.168.0.52', port=9090)
         self.rosbridge_client.run()
         self.coordinate_topic = roslibpy.Topic(self.rosbridge_client, '/object_coordinates', 'geometry_msgs/Point')
         self.direction_topic = roslibpy.Topic(self.rosbridge_client, '/object_direction', 'std_msgs/String')
         self.depth_topic = roslibpy.Topic(self.rosbridge_client, '/object_depth', 'std_msgs/String')
+        self.tag_exist_topic = roslibpy.Topic(self.rosbridge_client, '/tag_exist', 'std_msgs/String')
 
         # 读取相机内参
         camera_calib_path = os.path.join(package_share_directory, 'resource', 'ost.yaml')
@@ -54,8 +55,11 @@ class PokemonNode(Node):
         self.create_subscription(CompressedImage, "/camera/color/image_raw/compressed", self.image_callback, 10)
         self.create_subscription(CompressedImage, "/camera/depth/image_raw/compressed", self.depth_callback, 10)
         self.create_subscription(Imu, "/imu/data_raw", self.imu_callback, 10)
+        self.create_subscription(String, "/tag_name", self.tag_name_callback, 10)
         self.color_background = None
         self.depth_image = None
+        self.tag_exist = None
+        self.tag_name_from_node = None
 
     def imu_callback(self, msg):
         orientation_q = msg.orientation
@@ -68,10 +72,16 @@ class PokemonNode(Node):
         ])
         self.orientation_data.append(euler)
 
-    def image_callback(self, msg):
-        target_tag = "pikachu"  # 指定需要处理的标签
-        tolerance = 0.1  # 容忍度
+    # 取得 tag
+    def tag_name_callback(self, msg):
+        self.tag_name_from_node = msg.data
 
+
+    def image_callback(self, msg):
+        if self.tag_name_from_node != "None":
+            target_tag = self.tag_name_from_node
+        else:
+            target_tag = "None"
         if isinstance(msg, CompressedImage):
             np_arr = np.frombuffer(msg.data, np.uint8)
             frame = cv2.imdecode(np_arr, cv2.IMREAD_COLOR)
@@ -81,13 +91,21 @@ class PokemonNode(Node):
         results = self.model.predict(source=frame, verbose=False, device='cuda')
 
         color_background = np.zeros_like(frame)
-
+        found_target = False
         if results and len(results[0].boxes) > 0:
             for result in results:
                 for box in result.boxes:
                     label = self.model.names[int(box.cls[0])]
+                    x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+
+                    # 绘制检测框
+                    cv2.rectangle(frame, (x1, y1), (x2, y2), (255, 0, 0), 2)
+                    cv2.putText(frame, label, (x1, y1 - 10), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (255, 0, 0), 2)
+
                     if label == target_tag:  # 只处理特定标签的物体
-                        x1, y1, x2, y2 = map(int, box.xyxy[0].tolist())
+                        found_target = True
+                        tag_exist_msg = roslibpy.Message({'data': "1"})
+                        self.tag_exist_topic.publish(tag_exist_msg)
 
                         object_region = frame[y1:y2, x1:x2]
                         avg_color = cv2.mean(object_region)[:3]
@@ -96,53 +114,35 @@ class PokemonNode(Node):
                         color_background[y1:y2, x1:x2] = color_block
 
                         depth, point = self.get_depth_and_point_for_box(x1, y1, x2, y2)
+                        rect_width = 100
+                        rect_height = 100
+                        rect_center_x = frame.shape[1] // 2
+                        rect_center_y = frame.shape[0] // 2
+                        box_center_x = (x1 + x2) // 2
+                        box_center_y = (y1 + y2) // 2
+                        offset_x = (box_center_x - rect_center_x) / (frame.shape[1] // 10)
+                        offset_y = (rect_center_y - box_center_y) / (frame.shape[0] // 10)
+
+                        # 使用 x 和 y 轴刻度判断方向，并调整 y 轴的高度到 -1
+                        if abs(offset_x) < 1 and abs(offset_y +1) < 1:
+                            direction = "front"  # 物体在方格中心且 y 轴高度接近 -1
+                        elif abs(offset_x) > abs(offset_y + 1):
+                            if offset_x > 1:
+                                direction = "right"
+                            else:
+                                direction = "left"
+                        else:
+                            if offset_y < 0:
+                                direction = "down"
+                            else:
+                                direction = "up"
+                        direction_msg = roslibpy.Message({'data': direction})
+                        self.direction_topic.publish(direction_msg)
+
                         if depth is not None:
                             depth_in_meters = depth / 1000.0
-                            print(depth_in_meters)
-
-                            # 发布深度信息到新的topic
                             depth_msg = roslibpy.Message({'data': str(depth_in_meters)})
                             self.depth_topic.publish(depth_msg)
-
-                            if depth_in_meters < 0.2:
-                                rect_width = 100
-                                rect_height = 100
-                                rect_center_x = frame.shape[1] // 2 + 20
-                                rect_center_y = frame.shape[0] // 2 + 100
-
-                                box_center_x = (x1 + x2) // 2
-                                box_center_y = (y1 + y2) // 2
-
-                                if abs(box_center_x - rect_center_x) < rect_width // 2 and abs(box_center_y - rect_center_y) < rect_height // 2:
-                                    direction = "front"
-                                elif box_center_x < rect_center_x - rect_width // 2:
-                                    direction = "left"
-                                elif box_center_x > rect_center_x + rect_width // 2:
-                                    direction = "right"
-                                elif box_center_y < rect_center_y - rect_height // 2:
-                                    direction = "up"
-                                else:
-                                    direction = "down"
-                            else:
-                                frame_center_x = frame.shape[1] / 2
-                                frame_center_y = frame.shape[0] / 2
-                                up_tolerance_factor = 2.0
-                                front_offset_factor = 0.5
-
-                                if abs((x1 + x2) / 2 - frame_center_x) / frame.shape[1] < tolerance:
-                                    if abs((y1 + y2) / 2 - (frame_center_y - frame.shape[0] * front_offset_factor)) / frame.shape[0] < tolerance:
-                                        direction = "front"
-                                    elif (y1 + y2) / 2 < frame_center_y - frame.shape[0] * (up_tolerance_factor * tolerance):
-                                        direction = "up"
-                                    else:
-                                        direction = "down"
-                                elif (x1 + x2) / 2 < frame_center_x:
-                                    direction = "left"
-                                else:
-                                    direction = "right"
-
-                            direction_msg = roslibpy.Message({'data': direction})
-                            self.direction_topic.publish(direction_msg)
 
                             if point is not None:
                                 point_camera = np.dot(np.linalg.inv(self.camera_matrix), np.array([point[0], point[1], 1.0])) * depth_in_meters
@@ -161,24 +161,46 @@ class PokemonNode(Node):
                                     'z': float(point_bullet[2])
                                 })
                                 self.coordinate_topic.publish(roslibpy_point_msg)
+        if not found_target:
+            tag_exist_msg = roslibpy.Message({'data': "0"})
+            self.tag_exist_topic.publish(tag_exist_msg)
+            direction_msg = roslibpy.Message({'data': "stop"})
+            self.direction_topic.publish(direction_msg)
 
-        annotated_frame = results[0].plot()
+        # 绘制中心的y轴和x轴刻度
+        rect_center_x = frame.shape[1] // 2
+        rect_center_y = frame.shape[0] // 2
+        y_interval = frame.shape[0] // 10  # y轴每个刻度间隔
+        x_interval = frame.shape[1] // 10  # x轴每个刻度间隔
 
-        rect_width = 100
-        rect_height = 100
-        rect_center_x = annotated_frame.shape[1] // 2 + 20
-        rect_center_y = annotated_frame.shape[0] // 2 + 100
+        # 绘制Y轴刻度
+        for i in range(-5, 6):  # 从-5到+5
+            y_position = rect_center_y - i * y_interval
+            cv2.line(frame, (rect_center_x - 10, y_position), (rect_center_x + 10, y_position), (0, 255, 0), 2)
+            cv2.putText(frame, f"{i}", (rect_center_x + 15, y_position + 5),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
 
-        top_left = (rect_center_x - rect_width // 2, rect_center_y - rect_height // 2)
-        bottom_right = (rect_center_x + rect_width // 2, rect_center_y + rect_height // 2)
+        # 绘制X轴刻度
+        for i in range(-5, 6):  # 从-5到+5
+            x_position = rect_center_x + i * x_interval
+            cv2.line(frame, (x_position, rect_center_y - 10), (x_position, rect_center_y + 10), (0, 255, 0), 2)
+            cv2.putText(frame, f"{i}", (x_position - 10, rect_center_y - 15),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 255, 0), 2, cv2.LINE_AA)
 
-        cv2.rectangle(annotated_frame, top_left, bottom_right, color=(0, 255, 0), thickness=2)
-
-        detection_image = self.bridge.cv2_to_imgmsg(annotated_frame, encoding="bgr8")
+        # 发布处理后的图像
+        detection_image = self.bridge.cv2_to_imgmsg(frame, encoding="bgr8")
         self.detection_image_publisher_.publish(detection_image)
 
         color_image = self.bridge.cv2_to_imgmsg(color_background, encoding="bgr8")
         self.color_image_publisher_.publish(color_image)
+
+
+
+
+
+
+
+
 
     def depth_callback(self, msg):
         if isinstance(msg, CompressedImage):
